62d50f375ead2929e61f17b065d5619b634a5273
Garrett Middleton <garrettmiddleton@gmail.com>
2021-11-23 09:37:37 -0800

    added demo user

--- frontend/src/components/Comments/CreateCommentForm.js
+++ frontend/src/components/Comments/CreateCommentForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { createCommentThunk } from "../../store/comments";
-import { loadOnePost } from "../../store/singlePost";
+// import { loadOnePost } from "../../store/singlePost";
 import { getAllCommentsThunk } from "../../store/comments";
 import { thunkGetAllPosts } from "../../store/posts";
 
@@ -9,11 +9,10 @@ import { thunkGetAllPosts } from "../../store/posts";
 
 function CreateCommentForm({post}) {
   const dispatch = useDispatch();
-
-  const ownerId = useSelector((state) => state.session.user.id);
+  const ownerId = useSelector((state) => state?.session?.user?.id);
   const [content, setContent] = useState("");
-//   const comments = useSelector((state) => state.comments);
   const [valErrors, setValErrors] = useState([]);
+//   const comments = useSelector((state) => state.comments);
 //   const postId = posts[posts.id]?.Comments
       
   
