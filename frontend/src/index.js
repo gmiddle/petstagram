eaf19ccf8bc6a38da032a5f15740ab45ba7b577a
Garrett Middleton <garrettmiddleton@gmail.com>
2022-03-06 23:37:35 -0800

    adjusted spacing on index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,7 +15,6 @@ import * as sessionActions from "./store/session";
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
-
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
